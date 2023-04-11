2c94f1d8fadfd55766c0bc77d650736354c57966
Mc-zen <alphabetagamma11@hotmail.com>
2023-04-11 16:20:54 +0200

    [improve] wrap find_ht_circuit into class in order to avoid unnecessary creation of gurobi models and environments. Also many parameters and matrices can be updated only once for a set of paulis

--- src/grouper/pauli_grouper.h
+++ src/grouper/pauli_grouper.h
@@ -37,6 +37,41 @@ namespace Q {
 		return false;
 	}
 
+	/// @brief Check if given set of Paulis is measurable with a hardware tailored circuit using one
+	///        of the graphs specified. This 
+	/// 
+	///        NOTE: This version removes all graphs from the given list that cannot be used to diagonalize the Paulis. 
+	/// @param collection    Set of pauli operators to diagonalize
+	/// @param connectivity  Set of graphs to test for measurability. 
+	/// @return success
+	template<int n>
+	bool is_ht_measurable2(const std::vector<Pauli>& collection, std::vector<Graph<n>>& graphs, HTCircuitFinder& finder) {
+		finder.setOperators(collection);
+		//println("{}\n{}", collection, connectivity.getAdjacencyMatrix());
+		std::vector<int> doErase;
+		bool success = false;
+		for (const auto& graph : graphs) {
+			//println("{}", graph.getAdjacencyMatrix());
+			//if (auto result = findHTCircuit(graph, collection)) {
+			if (auto result = finder.findHTCircuit(graph)) {
+				//println("jo");
+				doErase.push_back(false);
+				//return true;
+				success = true;
+				if (collection.size() == 1 || graphs.size() == 1) return true;
+			}
+			else {
+				doErase.push_back(true);
+			}
+		}
+		if (success) {
+			std::erase_if(graphs, [&](const auto& graph) { return !finder.findHTCircuit(graph).has_value(); });
+		}
+		//std::cout << "no\n";
+		//println("no, {}", collection);
+		return success;
+	}
+
 
 	/// @brief Group Paulis of given hamiltonian into commuting subset that are diagonalizable
 	///        with the given hardware connectivity
@@ -46,28 +81,36 @@ namespace Q {
 	template<int n>
 	auto applyPauliGrouper(Hamiltonian& hamiltonian, const std::vector<Graph<n>>& graphs) {
 		println("Num graphs: {}", graphs.size());
+
+		HTCircuitFinder finder{ hamiltonian.numQubits };
+
 		// Sort by magnitude in descending order 
 		std::ranges::sort(hamiltonian.operators, [](const auto& a, const auto& b) {return std::abs(a.second) > std::abs(b.second); });
 		//std::ranges::sort(hamiltonian.operators, [](const auto& a, const auto& b) {return std::abs(a.first.pauliWeight()) < std::abs(b.first.pauliWeight()); });
 
-		std::vector<std::vector<Pauli>> collections;
+
+		using Collection = std::pair<std::vector<Pauli>, std::vector<Graph<n>>>;
+		std::vector<Collection> collections;
+		//std::vector<std::vector<Pauli>> collections;
 
 		int i{};
 		for (const auto& [pauli, coefficient] : hamiltonian.operators) {
 			bool found{};
-			for (auto& collection : collections) {
-				if (!commutesWithAll(collection, pauli)) continue;
+			for (Collection& collection : collections) {
+				if (!commutesWithAll(collection.first, pauli)) continue;
 
-				collection.push_back(pauli);
-				if (is_ht_measurable(collection, graphs)) {
+				collection.first.push_back(pauli);
+				if (is_ht_measurable2(collection.first, collection.second, finder)) {
 					found = true;
 					break;
 				}
-				collection.pop_back();
+				collection.first.pop_back();
 			}
 			if (!found) {
-				collections.insert(collections.end(), std::vector{ pauli });
+				collections.emplace(collections.end(), std::vector{ pauli }, graphs);
+				println("New group ({})", collections.size());
 			}
+			//std::cout << ++i<<" of " << hamiltonian.operators.size() << "\n";
 			println("{} of {}", ++i, hamiltonian.operators.size());
 		}
 		return collections;
--- src/q-library/find_ht_circuit.h
+++ src/q-library/find_ht_circuit.h
@@ -8,6 +8,8 @@
 #include <optional>
 #include <string_view>
 #include <fstream>
+#include <chrono>
+#include "formatting.h"
 
 namespace Q {
 	void qwe() { /**/ }
@@ -131,8 +133,6 @@ namespace Q {
 		}
 		return std::nullopt;
 	}
-
-	
 	/// @brief Find a Local Clifford (if it exists) that rotates a given stabilizer into a given graph state |Γ〉. 
 	/// @param graph    Graph that describes the graph state |Γ〉
 	/// @param RS       Stabilizer as a list of Pauli operators
@@ -150,6 +150,8 @@ namespace Q {
 		const auto Axz = Math::diag(generateSymbolVector(numQubits, "axz"));
 		const auto Azx = Math::diag(generateSymbolVector(numQubits, "azx"));
 		const auto Azz = Math::diag(generateSymbolVector(numQubits, "azz"));
+
+		auto t0 = std::chrono::high_resolution_clock::now();
 
 		Math::Matrix<int> R(numQubits, numOperatorsPerSet);
 		Math::Matrix<int> S(numQubits, numOperatorsPerSet);
@@ -228,8 +230,11 @@ namespace Q {
 				++i;
 			}
 
+			auto t1 = std::chrono::high_resolution_clock::now();
 
 			model.optimize();
+			auto t2 = std::chrono::high_resolution_clock::now();
+			//println("Time A: {}, Time B {}", t1 - t0, t2 - t1);
 			if (model.get(GRB_IntAttr_Status) != 2) { // failed
 				return std::nullopt;
 			}
@@ -251,6 +256,163 @@ namespace Q {
 		}
 		return std::nullopt;
 	}
+
+
+	class HTCircuitFinder {
+		GRBEnv env{ true };
+		std::unique_ptr<GRBModel> model;
+
+		std::vector<GRBVar> axxVars, axzVars, azxVars, azzVars;
+		std::vector<std::vector<GRBVar>*> aVars = { &axxVars,&axzVars,&azxVars,&azzVars };
+		Math::Matrix<Q::Term> Axx;
+		Math::Matrix<Q::Term> Axz;
+		Math::Matrix<Q::Term> Azx;
+		Math::Matrix<Q::Term> Azz;
+
+		Math::Matrix<int> R;
+		Math::Matrix<int> S;
+
+		Math::Matrix<Q::Term> C; // Axx * R + Axz * S
+		Math::Matrix<Q::Term> D; // Azx * R + Azz * S
+
+		int numQubits{};
+		int numOperatorsPerSet{};
+
+	public:
+		HTCircuitFinder(int numQubits, bool verbose=false) : numQubits(numQubits) {
+			env.set(GRB_IntParam_OutputFlag, verbose);
+			env.set("LogFile", "mip1.log");
+			env.start();
+
+			model = std::make_unique<GRBModel>(env);
+			model->setObjective(GRBLinExpr{ 0 }, GRB_MINIMIZE);
+
+			// Declare diagonal symbol matrices for blocks of symplectic matrix. 
+			Axx = Math::diag(generateSymbolVector(numQubits, "axx"));
+			Axz = Math::diag(generateSymbolVector(numQubits, "axz"));
+			Azx = Math::diag(generateSymbolVector(numQubits, "azx"));
+			Azz = Math::diag(generateSymbolVector(numQubits, "azz"));
+			// Add quadratic constraints
+			for (int i = 0; i < numQubits; ++i) {
+				axxVars.push_back(model->addVar(0, 1, 0, GRB_BINARY, "axx" + std::to_string(i)));
+				axzVars.push_back(model->addVar(0, 1, 0, GRB_BINARY, "axz" + std::to_string(i)));
+				azxVars.push_back(model->addVar(0, 1, 0, GRB_BINARY, "azx" + std::to_string(i)));
+				azzVars.push_back(model->addVar(0, 1, 0, GRB_BINARY, "azz" + std::to_string(i)));
+
+				model->addQConstr(axxVars[i] * azzVars[i] + axzVars[i] * azxVars[i] == 1, "qc" + std::to_string(i));
+			}
+		}
+
+		void setOperators(auto&& RS) {
+			numOperatorsPerSet = RS.size();
+			R.resize(numQubits, numOperatorsPerSet);
+			S.resize(numQubits, numOperatorsPerSet);
+			for (size_t i = 0; i < numQubits; ++i) {
+				for (size_t j = 0; j < numOperatorsPerSet; ++j) {
+					R(i, j) = RS[j].x(i);
+					S(i, j) = RS[j].z(i);
+				}
+			}
+			C = Axx * R + Axz * S;
+			D = Azx * R + Azz * S;
+		}
+
+		/// @brief Find a Local Clifford (if it exists) that rotates a given stabilizer into a given graph state |Γ〉. 
+		/// @param graph    Graph that describes the graph state |Γ〉
+		/// @param RS       Stabilizer as a list of Pauli operators
+		/// @param verbose  If set to true, the generated equations are printed to stdout
+		/// @return         If successfull, a list of symplectic 2x2 matrices, corresponding to the 6 single-qubit Clifford gates
+		std::optional<std::vector<BinaryCliffordGate>> findHTCircuit(const Graph<>& graph, bool verbose = false) {
+			using std::cout;
+			auto numEqs = numQubits * numOperatorsPerSet;
+
+
+			auto t0 = std::chrono::high_resolution_clock::now();
+
+			const auto& adjacencyMatrix = graph.getAdjacencyMatrix();
+			const auto lhs = simplified(adjacencyMatrix.cast<int>() * C + D);
+			auto t1 = std::chrono::high_resolution_clock::now();
+
+			if (verbose)
+				std::cout << R << S << lhs;
+
+			try {
+
+
+				auto toGRBLinExpression = [&](const Term& t) {
+					auto getVar = [&](const Variable& t) -> GRBVar& {
+						return aVars[((t.name[1] == 'z') * 2) + (t.name[2] == 'z')]->operator[](t.name[3] - '0');
+					};
+					GRBLinExpr expr;
+					const auto simplifiedTerm = simplified(t);
+					for (const auto& c : simplifiedTerm.numbers) {
+						expr += c.real();
+					}
+					for (const auto& c : simplifiedTerm.variables) {
+						expr += getVar(c);
+					}
+					assert(simplifiedTerm.products.empty() && "Expression is not linear");
+					for (const auto& c : simplifiedTerm.products) {
+						assert(c.sums.empty());
+						GRBLinExpr p;
+
+						for (const auto& d : c.numbers) {
+							p *= d.real();
+						}
+						for (const auto& d : c.variables) {
+							//p *= getVar(d);
+						}
+						expr += p;
+					}
+					return expr;
+				};
+
+				// dummy variables (used for rhs to ensure that lhs is multiple of 2 (corresponding to 0 in binary field) for each entry
+				std::vector<GRBVar> dummyVars;
+				for (int i = 0; i < numEqs; ++i) {
+					dummyVars.push_back(model->addVar(-1000, 1000, 0.0, GRB_INTEGER, ""));
+				}
+
+				std::vector<GRBConstr> constraints;
+				int i{};
+				for (const auto& row : lhs) {
+					constraints.push_back(model->addConstr(toGRBLinExpression(row) * 0.5 == dummyVars[i], "i" + std::to_string(i)));
+					++i;
+				}
+
+				model->optimize();
+				auto t2 = std::chrono::high_resolution_clock::now();
+				//println("Time A: {}, Time B {}", t1 - t0, t2 - t1);
+
+
+				for (auto& var : dummyVars) {
+					model->remove(var);
+				}
+				for (auto& constr : constraints) {
+					model->remove(constr);
+				}
+				if (model->get(GRB_IntAttr_Status) != 2) { // failed
+					return std::nullopt;
+				}
+				//model.write("model.lp");
+				std::vector<BinaryCliffordGate> singleQubitLayer(numQubits);
+				for (int i = 0; i < numQubits; ++i) {
+					singleQubitLayer[i] = { axxVars[i].get(GRB_DoubleAttr_X) ,axzVars[i].get(GRB_DoubleAttr_X) ,azxVars[i].get(GRB_DoubleAttr_X) ,azzVars[i].get(GRB_DoubleAttr_X) };
+					if (verbose)
+						cout << "U_" << i << "\n" << singleQubitLayer[i];
+				}
+				return singleQubitLayer;
+			}
+			catch (const GRBException& e) {
+				cout << "Error code = " << e.getErrorCode() << '\n';
+				cout << e.getMessage() << '\n';
+			}
+			catch (...) {
+				cout << "Exception during optimization" << '\n';
+			}
+			return std::nullopt;
+		}
+	};
 
 
 
