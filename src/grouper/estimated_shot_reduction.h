d93d3f310d17bd34c6622fe5e3d5f069ac2e0f66
Mc-Zen@a <alphabetagamma11@hotmail.com>
2023-05-15 19:26:51 +0200

    [add] shot reduction estimation

---
+++ src/grouper/estimated_shot_reduction.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <algorithm>
+#include "hamiltonian.h"
+
+
+namespace Q {
+
+	double estimated_shot_reduction(const Hamiltonian& hamiltonian, const std::vector<CollectionWithGraph>& grouping) {
+		double numerator{};
+		double denominator{};
+
+		for (const auto& group : grouping) {
+			double denominatorTerm{};
+			for (const auto& pauli : group.paulis) {
+				auto coefficient = std::get<double>(*std::ranges::find(hamiltonian.operators, pauli, [](const auto& a) { return std::get<0>(a); }));
+				double absolute = std::abs(coefficient);
+				numerator += absolute;
+				denominatorTerm += absolute * absolute;
+			}
+			denominator += std::sqrt(denominatorTerm);
+		}
+		return numerator * numerator / (denominator * denominator);
+	}
+}
