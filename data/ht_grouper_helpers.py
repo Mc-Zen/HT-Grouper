7dda0f59879c0860bd169b8a6a67fa432c4cf28e
Mc-Zen@a <alphabetagamma11@hotmail.com>
2023-05-19 11:48:40 +0200

    [add] python helpers HamiltonianExperiment + result evaluation

--- data/ht_grouper_helpers.py
+++ data/ht_grouper_helpers.py
@@ -1,5 +1,9 @@
+import numpy as np
+import qiskit
+from qiskit.quantum_info import Pauli
+from qiskit.result import Result
 import json
-from typing import Dict, List
+from typing import Dict, List, Optional, Sequence, Union
 from qiskit import QuantumCircuit
 from qiskit.transpiler import PassManager
 from qiskit.transpiler.passes import InverseCancellation
@@ -151,13 +155,153 @@ def R_hat(grouping: List[dict], hamiltonian: Dict[str, float]) -> float:
     return (numerator / denominator) ** 2
 
 
-hamiltonian = read_hamiltonian("hamiltonians/H_chain.json")
+Bitstring = np.int64
 
-grouping = read_grouping("groupings/ham2_grouping.json")
-grouping = read_grouping("groupings/H_chain_grouping.json")
 
-# circuits = generate_readout_circuits(grouping)
-# for circuit in circuits:
-#     print(circuit)
+class BinaryResult:
+    """
+    Class for storing a single quantum circuit result with result
+    signature and count. 
 
-print(R_hat(grouping, hamiltonian))
+    The result signature is encoded in a binary string in little-endian
+    (least-significant bit represents the zeroth quantum bit result). 
+
+    """
+
+    __slots__ = ("bitstring", "count")
+
+    def __init__(self, bitstring: Union[Bitstring, int], count: int):
+        self.bitstring = Bitstring(bitstring)
+        self.count = count
+
+    def __eq__(self, other) -> bool:
+        return self.bitstring == other.bitstring and self.count == other.count
+
+    def __str__(self, num_qubits=0) -> str:
+        return f"{self.bitstring:0>{num_qubits}b}: {self.count}"
+
+    def __repr__(self) -> str:
+        return f"BinaryResult({self.bitstring}, {self.count})"
+
+
+class CircuitResult:
+    """
+    Class encapsuling results from one quantum circuit, similar to 
+    :class:`qiskit.result.ExperimentResultData` but the keys are not stored
+    using hexadecimal strings but binary bitstrings which allows for
+    some performance improvements when evaluating readout results. . 
+
+    """
+
+    __slots__ = ("results", "num_qubits")
+
+    def __init__(self, counts: Dict[str, int], qubits: Optional[Sequence[int]] = None):
+        """Create a CircuitResult from a dictionary as returned by `qiskit.result.get_counts()`.
+        The keys are little-endian bitstrings, i.e. the zeroth register is at the rightmost 
+        position in the string.
+
+        E.g., ``{"001": 5, "010": 4}`` to denote a result where the outcome with only the zeroth 
+        qubit being 1 occured 5 times and the outcome of the first qubit begin 1 occured 5 times. 
+
+        Optionally, the results on only a selection of qubits can be extracted (marginalization).  
+        Example: 
+
+        >>> CircuitResult({"101": 9, "001": 4}, [0, 2])
+
+        will result in storing ``{"11": 9, "01": 4}``. 
+
+        Parameters
+        ----------
+        counts : Dict[str, int]
+            Outcome/count dictionary
+        qubits : Optional[Sequence[int]], optional
+            If specified, only the given qubits are extracted. 
+        """
+        self.results: List[BinaryResult] = []
+
+        if qubits is None:
+            if len(counts) != 0:
+                self.num_qubits = len(next(iter(counts)).replace(" ", ""))
+            for key, value in counts.items():
+                self.results.append(BinaryResult(Bitstring(int(key.replace(" ", ""), 2)), value))
+
+        else:
+            self.num_qubits = len(qubits)
+            for key, value in counts.items():
+                key = key.replace(" ", "")  # might contain spaces to separate registers
+                key = "".join(key[index] for index in qubits)
+                self.results.append(BinaryResult(Bitstring(int(key, 2)), value))
+
+    def __str__(self) -> str:
+        return "CircuitResult(" + ", ".join(result.__str__(self.num_qubits) for result in self.results) + ")"
+
+
+class HamiltonianExperiment:
+
+    def __init__(self, preparation_circuit: QuantumCircuit, grouping: List[dict], hamiltonian: Dict[str, float]):
+        self.preparation_circuit = preparation_circuit
+        self.grouping = grouping
+        self.hamiltonian = hamiltonian
+        self.num_qubits = len(grouping[0]["operators"][0])
+        assert self.num_qubits == preparation_circuit.num_qubits, "Number of qubits do not match for preparation circuit and hamiltonian"
+
+    def get_readout_circuits(self) -> List[QuantumCircuit]:
+        if not hasattr(self, "readout_circuits"):
+            self.readout_circuits = generate_readout_circuits(self.grouping)
+        return self.readout_circuits
+
+    def get_circuits(self) -> List[QuantumCircuit]:
+        if not hasattr(self, "circuits"):
+            readout_circuits = self.get_readout_circuits()
+            self.circuits = []
+            for readout_circuit in readout_circuits:
+                qc = self.preparation_circuit.compose(readout_circuit)
+                qc.measure_all()
+                self.circuits.append(qc)
+        return self.circuits
+
+    def simulate(self, shots):
+        circuits = self.get_circuits()
+        backend_sim = qiskit.Aer.get_backend("qasm_simulator")
+        job_sim = qiskit.execute(circuits, backend=backend_sim, shots=shots)
+        return job_sim.result()
+
+    def evaluate(self, result: Result) -> Dict[Pauli, float]:
+        all_counts = result.get_counts()
+        expectation_values: Dict[Pauli, float] = {}
+
+        for group, readout_circuit, counts in zip(self.grouping, self.get_readout_circuits(), all_counts):
+            circuit_result = CircuitResult(counts)
+            inverse_circuit = readout_circuit.inverse()
+            for pauli_string in group["operators"]:
+                pauli = Pauli(pauli_string)
+                pauli_z = pauli.evolve(inverse_circuit)
+                assert pauli_z.phase == 2 or pauli_z.phase == 0
+
+                expectation_value = _compute_expectation_value(circuit_result, create_bitstring_from_nparray(pauli_z.z))
+                if pauli_z.phase == 2:
+                    expectation_value *= -1
+                expectation_values[pauli] = expectation_value
+
+        expectation_values[Pauli("I" * self.num_qubits)] = 1
+        return expectation_values
+
+
+def create_bitstring_from_nparray(arr: np.ndarray) -> Bitstring:
+    bitstring = Bitstring(0)
+    for i in range(len(arr)):
+        if arr[i]:
+            bitstring |= (1 << i)
+    return bitstring
+
+
+def _compute_expectation_value(circuit_result: CircuitResult, s: Bitstring):
+    expectation_value: int = 0
+    total_count: int = 0
+    for result in circuit_result.results:
+        if (s & result.bitstring).bit_count() & 1:
+            expectation_value -= result.count
+        else:
+            expectation_value += result.count
+        total_count += result.count
+    return expectation_value / total_count
